8cd647a798766c162748881dedbe917809051120
Julius <juliusngigim@gmail.com>
2022-01-21 16:08:39 +0300

    Remove Pay Now Button, Launch Payment pop-up on save and continue

--- app/assets/javascripts/spree/frontend/paystack_checkout.js
+++ app/assets/javascripts/spree/frontend/paystack_checkout.js
@@ -1,35 +1,41 @@
-  function payNowWithPaystack(event){
-      const checkedPaymentMethod = $('#payment-method-fields input[type="radio"]:checked').val();
-      const paystackPaymentMethodId = $("#paystack_checkout_payload").data("paymentgateway");
-
-      if (paystackPaymentMethodId.toString() === checkedPaymentMethod) {
-          const currentOrder = $("#paystack_checkout_payload").data("current_order_id");
-          const orderTotal = $("#paystack_checkout_payload").data("order_total");
-          const email = $("#paystack_checkout_payload").data("billing_email");
-          const currency = $("#paystack_checkout_payload").data("currency");
-          payWithPaystack(event, currentOrder, orderTotal, email, currency);
-      }
-  }
-
-  function payWithPaystack(event, orderId, amount, email, currency) {
-      event.preventDefault();
-      const secret = $("#paystack_checkout_payload").data("secret");
-      const amount_kobo = String(amount) * 100
-      let handler = PaystackPop.setup({
-        key: secret,
-        email: email,
-        currency:currency,
-        amount: amount_kobo,
-        ref: orderId,
-        onClose: function(){
-            return false;
-        },
-        callback: function(){
-            $('#checkout_form_payment').submit();
+$( document ).ready(function() {
+    $('#checkout_form_payment').submit(function () {
+        const checkedPaymentMethod = $('#payment-method-fields input[type="radio"]:checked').val();
+        const paystackPaymentMethodId = $("#paystack_checkout_payload").data("paymentgateway");
+        if (paystackPaymentMethodId.toString() === checkedPaymentMethod) {
+            payNowWithPaystack(window.event);
             return true;
         }
-      });
+    });
+});
 
-      handler.openIframe();
+function payNowWithPaystack(event){
+    const currentOrder = $("#paystack_checkout_payload").data("current_order_id");
+    const orderTotal = $("#paystack_checkout_payload").data("order_total");
+    const email = $("#paystack_checkout_payload").data("billing_email");
+    const currency = $("#paystack_checkout_payload").data("currency");
+    payWithPaystack(event, currentOrder, orderTotal, email, currency);
+}
 
-  }
+function payWithPaystack(event, orderId, amount, email, currency) {
+    event.preventDefault();
+    const secret = $("#paystack_checkout_payload").data("secret");
+    const amount_kobo = String(amount) * 100
+    let handler = PaystackPop.setup({
+    key: secret,
+    email: email,
+    currency:currency,
+    amount: amount_kobo,
+    ref: orderId,
+    onClose: function(){
+        return false;
+    },
+    callback: function(){
+        $('#checkout_form_payment').submit();
+        return true;
+    }
+  });
+
+  handler.openIframe();
+
+}
