11937142a066b65c45f6f5c6ada10b34c7c19dc1
Julius <juliusngigim@gmail.com>
2022-01-24 17:20:00 +0300

    Attempt to fix double submission of the checkout_form_payment

--- app/assets/javascripts/spree/frontend/paystack_checkout.js
+++ app/assets/javascripts/spree/frontend/paystack_checkout.js
@@ -3,7 +3,9 @@ $( document ).ready(function() {
         const checkedPaymentMethod = $('#payment-method-fields input[type="radio"]:checked').val();
         const paystackPaymentMethodId = $("#paystack_checkout_payload").data("paymentgateway");
         if (paystackPaymentMethodId.toString() === checkedPaymentMethod) {
-            payNowWithPaystack(window.event);
+            if (typeof window.event.data === "undefined") {
+                payNowWithPaystack(window.event);
+            }
             return true;
         }
     });
@@ -22,20 +24,17 @@ function payWithPaystack(event, orderId, amount, email, currency) {
     const secret = $("#paystack_checkout_payload").data("secret");
     const amount_kobo = String(amount) * 100
     let handler = PaystackPop.setup({
-    key: secret,
-    email: email,
-    currency:currency,
-    amount: amount_kobo,
-    ref: orderId,
-    onClose: function(){
-        return false;
-    },
-    callback: function(){
-        $('#checkout_form_payment').submit();
-        return true;
-    }
-  });
-
-  handler.openIframe();
-
+        key: secret,
+        email: email,
+        currency:currency,
+        amount: amount_kobo,
+        ref: orderId,
+        onClose: function(){
+            return false;
+        },
+        callback: function() {
+            $('#checkout_form_payment').submit();
+        }
+    });
+    handler.openIframe();
 }
